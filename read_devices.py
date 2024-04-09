7dbed98f1408ebc626fa855531cc6d3bb0e80841
fancy <fancyfsz@qq.com>
2024-04-09 18:32:43 +0800

    查询设备列表补一个limit的查询参数，否则它默认只查出来20台设备

--- read_devices.py
+++ read_devices.py
@@ -4,12 +4,13 @@ from appstoreconnect import createASCToken, get, post
 
 
 def readDevices():
-    url = 'https://api.appstoreconnect.apple.com/v1/devices'
+    url = 'https://api.appstoreconnect.apple.com/v1/devices?limit=200'
 
     response = get(url)
     if response.status_code == 200:
         print('id,name,udid,model,status')
         device_data = response.json()
+        # print(device_data)
         devices = device_data['data']
         for device in devices:
             device_attributes = device['attributes']
